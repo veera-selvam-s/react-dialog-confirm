ebf828d65e513bfd5636b4d7f92de19050ea1366
Veera Selvam Sagayaraj <veeraselvam@mallow-tech.com>
2023-03-18 14:12:12 +0530

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import babel from 'rollup-plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
-import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss';
+const babel = require('rollup-plugin-babel');
+const resolve = require('@rollup/plugin-node-resolve');
+const external = require('rollup-plugin-peer-deps-external');
+const { terser } = require('rollup-plugin-terser');
+const postcss = require('rollup-plugin-postcss');
 
-export default [
+module.exports = [
   {
     input:'./src/index.js',
     output:[
